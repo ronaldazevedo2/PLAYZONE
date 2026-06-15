ee8962563434cc87399bda545254a7692dea2bad
ronaldazevedo2 <senac.ronaldazevedo@gmail.com>
2026-06-15 20:35:29 -0300

    excluimos

--- src/BaseApi.Application/Autenticacao/Commands/RedefinirSenha/RedefinirSenhaCommand.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace BaseApi.Application.Autenticacao.Commands.RedefinirSenha;
-
-/// <summary>
-/// Command para redefinir a senha usando o token recebido por e-mail.
-/// O token tem validade de 2 horas e é invalidado após o uso.
-/// </summary>
-public record RedefinirSenhaCommand(string Token, string NovaSenha, string ConfirmacaoSenha) : IRequest<Unit>;
